c5a056f5fec136b2bd52215b328b963967f41f1f
Irina <irina_bay84@mail.ru>
2022-03-27 16:39:46 +0500

    kino

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 "use strict";
 
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5`);
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
 
-const user = `Иван`;
-alert(`Привет,${user}!`);
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+
+    const a = prompt('Один из последних просмотренных фильмов?',''),
+          b = prompt('На сколько оцените его?',''),
+          c = prompt('Один из последних просмотренных фильмов?',''),
+          d = prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
